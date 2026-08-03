6d1ad9c082d4cb5b0694cd8fc5a960b7a3a6541c
Logan Kennedy <kennedylogan22@gmail.com>
2026-08-03 14:38:31 -0700

    feat: 🎸 add dynamo db table, receipts bucket, cognito user pool

---
+++ infra/lib/data-auth-stack.ts
@@ -0,0 +1,82 @@
+import * as cdk from 'aws-cdk-lib';
+import * as cognito from 'aws-cdk-lib/aws-cognito';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import type { Construct } from 'constructs';
+
+/** Origins allowed to upload receipt images directly to S3 via presigned URLs. */
+const RECEIPT_UPLOAD_ORIGINS = ['http://localhost:5173'];
+
+/**
+ * Stateful resources: the data store, receipt image storage, and the user directory.
+ *
+ * Everything here is retained on stack deletion because these hold real user data and
+ * should never be destroyed by a `cdk destroy`.
+ */
+export class DataAuthStack extends cdk.Stack {
+  readonly table: dynamodb.Table;
+  readonly receiptsBucket: s3.Bucket;
+  readonly userPool: cognito.UserPool;
+  readonly userPoolClient: cognito.UserPoolClient;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    this.table = new dynamodb.Table(this, 'Table', {
+      partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      pointInTimeRecoverySpecification: { pointInTimeRecoveryEnabled: true },
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    this.receiptsBucket = new s3.Bucket(this, 'ReceiptsBucket', {
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+      encryption: s3.BucketEncryption.S3_MANAGED,
+      enforceSSL: true,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+      cors: [
+        {
+          allowedMethods: [s3.HttpMethods.PUT],
+          allowedOrigins: RECEIPT_UPLOAD_ORIGINS,
+          allowedHeaders: ['*'],
+          maxAge: 3000,
+        },
+      ],
+    });
+
+    this.userPool = new cognito.UserPool(this, 'UserPool', {
+      selfSignUpEnabled: true,
+      signInAliases: { email: true },
+      autoVerify: { email: true },
+      standardAttributes: {
+        email: { required: true, mutable: false },
+      },
+      passwordPolicy: {
+        minLength: 12,
+        requireLowercase: true,
+        requireUppercase: true,
+        requireDigits: true,
+        requireSymbols: false,
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      removalPolicy: cdk.RemovalPolicy.RETAIN,
+    });
+
+    // Public SPA client: no secret, SRP so the password never leaves the browser.
+    this.userPoolClient = this.userPool.addClient('WebClient', {
+      authFlows: { userSrp: true },
+      preventUserExistenceErrors: true,
+      accessTokenValidity: cdk.Duration.hours(1),
+      idTokenValidity: cdk.Duration.hours(1),
+      refreshTokenValidity: cdk.Duration.days(30),
+    });
+
+    new cdk.CfnOutput(this, 'TableName', { value: this.table.tableName });
+    new cdk.CfnOutput(this, 'ReceiptsBucketName', { value: this.receiptsBucket.bucketName });
+    new cdk.CfnOutput(this, 'UserPoolId', { value: this.userPool.userPoolId });
+    new cdk.CfnOutput(this, 'UserPoolClientId', {
+      value: this.userPoolClient.userPoolClientId,
+    });
+  }
+}
